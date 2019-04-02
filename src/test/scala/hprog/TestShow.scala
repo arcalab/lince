c76564cf4c61aae4a8800fa501d8661d1e211075
Jose Proenca <jose@proenca.org>
2019-04-02 09:44:59 +0100

    adapted tests for the new pretty printer

--- src/test/scala/hprog/TestShow.scala
+++ src/test/scala/hprog/TestShow.scala
@@ -18,9 +18,9 @@ class TestShow extends FlatSpec {
   testPrint(ex3, "x' = 2, y' = 3")
   testPrint(ex4, "x' = 2\ny' = 3")
   testPrint(ex5, "x := 0\nx' = 2, y' = 3 & 34\nx' = 2 & x > 2")
-  testPrint(ex6, "x' = 1 & x > 3 x /\\ x < 5")
-  testPrint(ex7, "p' = v, v' = g & p <= 0 /\\ v <= 0\nv := -0.5 v")
-  testPrint(ex8, "x' = 1 & (x > 3 x /\\ x < 5) \\/ y >= 3")
-  testPrint(ex9, "x' = 1 & x > 3 x /\\ (x < 5 \\/ y >= 3)")
+  testPrint(ex6, "x' = 1 & x > 3*x /\\ x < 5")
+  testPrint(ex7, "p' = v, v' = g & p <= 0 /\\ v <= 0\nv := -0.5*v")
+  testPrint(ex8, "x' = 1 & (x > 3*x /\\ x < 5) \\/ y >= 3")
+  testPrint(ex9, "x' = 1 & x > 3*x /\\ (x < 5 \\/ y >= 3)")
 
 }
