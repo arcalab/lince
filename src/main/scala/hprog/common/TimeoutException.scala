b0818c518a91a412e8380796ee1630fc5160d07d
Jose Proenca <jose@proenca.org>
2019-10-17 17:05:18 +0100

    Timeout message - more positive

--- src/main/scala/hprog/common/TimeoutException.scala
+++ src/main/scala/hprog/common/TimeoutException.scala
@@ -1,4 +1,3 @@
 package hprog.common
 
-class TimeoutException(s:String) extends RuntimeException(s)
-
+class TimeoutException(s:String) extends RuntimeException("Timeout while waiting for Sage. Please reload.")
