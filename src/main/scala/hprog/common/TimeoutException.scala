804539f4d850736c019922d9c6cdfbbe0843d171
Jose Proenca <jose@proenca.org>
2019-10-21 12:15:15 +0100

    updated timeout message

--- src/main/scala/hprog/common/TimeoutException.scala
+++ src/main/scala/hprog/common/TimeoutException.scala
@@ -1,3 +1,3 @@
 package hprog.common
 
-class TimeoutException(s:String) extends RuntimeException("Timeout while waiting for Sage. Please reload.")
+class TimeoutException(s:String) extends RuntimeException("Stopped waiting for Sage. This timeout is normal in the first execution of Lince, because the associated systems need to be started. Please try again.")
