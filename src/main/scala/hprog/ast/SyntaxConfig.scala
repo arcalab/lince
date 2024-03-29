aa9e2a054965d812fc9f6e5c9dbed693a99474ba
Daniel Mendes <pdaniel.lm@gmail.com>
2024-03-29 16:03:21 +0000

    remove comments

--- src/main/scala/hprog/ast/SyntaxConfig.scala
+++ src/main/scala/hprog/ast/SyntaxConfig.scala
@@ -1,27 +1,3 @@
-/*package hprog.ast
-import Syntax._
-
-sealed abstract class SyntaxConfig   
-
-object SyntaxConfig {
-
-  /** An atomic program is a list of variables, a int representing the max time and a int representing max iterations //COnfig */
-  case class SyntaxConfig(axis:AxisList,maxTime:MaxTime,maxIterations:MaxIterations) 
-
-  /* AxisList is a lsit of strings that represents a axis' variables in a Lince Program */
-  case class AxisList(v:List[Var]) 
-
-  /* Var is a string that represents a variable in a Lince Program */
-  case class Var(v:String) 
-
-  /* Value is a double that represents the max time or max iterations that a lince program can run */
-  case class MaxTime(v:Double) 
-
-   /* Value is a double that represents the max time or max iterations that a lince program can run */
-  case class MaxIterations(v:Int) 
-
-}*/
-
 package hprog.ast
 import Syntax._
 
