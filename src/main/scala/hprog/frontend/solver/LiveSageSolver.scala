ff8a939ec3cab596d43e77935a395e59ee7092ee
José Proença <jose@proenca.org>
2024-08-24 01:32:01 +0200

    commented debug printlns

--- src/main/scala/hprog/frontend/solver/LiveSageSolver.scala
+++ src/main/scala/hprog/frontend/solver/LiveSageSolver.scala
@@ -137,11 +137,11 @@ class LiveSageSolver(path:String) extends StaticSageSolver {
     val instructions = genSage(eqs) // cria uma string das equações diferenciais para enviar para o Sage (ex: _t_=var('_t_'), function('...'))
     //println("eqs_withoutShow:",eqs)
     //println("eqs:",Show(eqs))
-    println("genSage:",instructions)
+//    println("genSage:",instructions)
     //debug(()=>s"solving: ${Show(eqs)}")
     val rep = askSage(instructions)
    // println("olaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    println("askSage:",rep)
+//    println("askSage:",rep)
    // println("rep.get.contains(g1634):",rep.get.contains("g1634"))
     if (rep.get.contains("g1634")) {
      // println("FIND ERROR")
